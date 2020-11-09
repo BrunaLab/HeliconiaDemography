ce081e78347058895344220a1d56cc42ce556ec5
Aariq <scottericr@gmail.com>
2020-11-09 12:20:56 -0500

    remove tests for functions that are no longer used.

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -6,26 +6,4 @@ test_that("matrix iteration works", {
   expect_equal(A %*% A %*% A, A %^% 3)
 })
 
-test_that("rectangle masks are unique", {
-  x <- mat_rect(D = 3, nrow = 10, ncol = 10)
-  l <- vector()
-  for(i in 1:length(x)) {
-    for(j in 1:length(x)) {
-      o <- all(x[[i]] == x[[j]]) & i!=j
-      l <- c(l, o)
-    }
-  }
-  expect_false(any(l))
-}) 
 
-test_that("diamond masks are unique", {
-  x <- mat_diamond(D = 3, nrow = 10, ncol = 10)
-  l <- vector()
-  for(i in 1:length(x)) {
-    for(j in 1:length(x)) {
-      o <- all(x[[i]] == x[[j]]) & i!=j
-      l <- c(l, o)
-    }
-  }
-  expect_false(any(l))
-}) 
