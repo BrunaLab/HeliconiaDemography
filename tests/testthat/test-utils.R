bcff9c546062551eb101a78fdcb9246ee73cf9dd
Aariq <scottericr@gmail.com>
2020-11-10 15:44:54 -0500

    added test that nearest() works with dplyr

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,11 +1,11 @@
 # library(here)
 # source(here("R", "utils.R"))
+library(dplyr)
 test_that("matrix iteration works", {
   A <- matrix(1:9, nrow = 3)
   expect_equal(A %*% A, A %^% 2)
   expect_equal(A %*% A %*% A, A %^% 3)
 })
-
 
 test_that("eu_dist() works", {
   x = c(0,0)
@@ -17,4 +17,5 @@ test_that("nearest() helper works", {
   x <- seq(1, 10, 0.3)
   expect_equal(which(nearest(x, 5)), 14)
   expect_equal(x[nearest(x, 5)], 4.9)
+  expect_s3_class(tibble(x) %>% filter(nearest(x, 5)), "tbl_df")
 })
