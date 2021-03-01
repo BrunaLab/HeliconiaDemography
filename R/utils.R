f10d91e216a1db7fdb4407dec5027a98e1d799d2
Aariq <scottericr@gmail.com>
2021-03-01 13:52:32 -0500

    extreme drought annotation now goes to botom of plot, regardless of expansion.

--- R/utils.R
+++ R/utils.R
@@ -68,9 +68,8 @@ nearest <- function(x, val) {
 #' annotate_spei(p)
 #' }
 annotate_spei <- function(p, ...) {
-  ymin <- min(ggplot2::ggplot_build(p)$layout$panel_scales_y[[1]]$range$range[1], -2.5)
   spei_rects <- list(
-    annotate(geom = "rect", xmin = -Inf, xmax = Inf, ymin = ymin, ymax = -2, fill = "#e31a1c", ...),
+    annotate(geom = "rect", xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = -2, fill = "#e31a1c", ...),
     annotate(geom = "rect", xmin = -Inf, xmax = Inf, ymin = -2, ymax = -1.5, fill = "#fd8d3c", ...),
     annotate(geom = "rect", xmin = -Inf, xmax = Inf, ymin = -1.5, ymax = -1, fill = "#fecc5c", ...),
     annotate(geom = "rect", xmin = -Inf, xmax = Inf, ymin = -1, ymax = 0, fill = "#ffffb2", ...) 
