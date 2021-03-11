8a11df1daa02616fe404eb4a1ed8d64586f30f66
Aariq <scottericr@gmail.com>
2021-03-11 16:46:02 -0500

    converted exploratory and descriptive plots to targets

---
+++ R/normals.R
@@ -0,0 +1,44 @@
+#' Return a tibble of temperature and precipitation normals for Manaus
+#' 
+#' Temperature and precipitation normals from 1981-2010 for the Manaus INMET weather station. 
+#' @source Data from INMET https://portal.inmet.gov.br/normais. 
+#' @return a tibble of climate normals
+#' @export
+#'
+#' @examples
+normals_data <- function() {
+  tibble(
+    month = 1:12,
+    temp_mean = c(26.30, 26.30, 26.30, 26.40, 26.60, 26.70, 27.00, 27.60, 28.00, 28.00, 27.60, 26.90),
+    temp_min  = c(23.10, 23.10, 23.20, 23.20, 23.40, 23.00, 23.10, 23.40, 23.70, 23.90, 23.70, 23.50),
+    temp_max  = c(30.90, 30.80, 30.90, 31.00, 31.10, 31.40, 32.10, 33.10, 33.50, 33.40, 32.60, 31.70),
+    precip    = c(287.0, 295.1, 300.0, 319.0, 246.9, 118.3, 75.40, 64.30, 76.30, 104.1, 169.2, 245.6)
+  )
+}
+
+
+plot_normals <- function(normals, save = TRUE) {
+  precip <-
+    ggplot(normals, aes(x = as.factor(month), y = precip)) + 
+    geom_col(fill = "darkblue") +
+    scale_y_continuous("Precipitation (mm)", expand = expansion(mult = c(0.05, 0.1))) +
+    theme_bw()
+  
+  temp <- 
+    ggplot(normals, aes(x = month)) +
+    geom_line(aes(y = temp_mean), color = "red") +
+    geom_line(aes(y = temp_min), color = "red", linetype = 2) +
+    geom_line(aes(y = temp_max), color = "red", linetype = 2) +
+    scale_x_continuous("Month", breaks = 1:12) +
+    scale_y_continuous("Temperature (ºC)", expand = expansion(mult = 0.3)) +
+    theme_bw()
+  
+  p <- 
+    (precip + theme(axis.title.x = element_blank(), axis.text.x = element_blank()))/
+    (temp) + plot_annotation(tag_levels = "a", tag_suffix = ")")
+  
+  if(save == TRUE) {
+    ggsave(here("analysis", "figures", "manaus_normals.png"), p)
+  }
+  return(p)
+}
--- _targets.R
+++ _targets.R
@@ -10,12 +10,14 @@ tar_option_set()
 
 ## tar_plan supports drake-style targets and also tar_target()
 tar_plan(
+  # Prep SPEI data
   maxlag = 36,
   tar_target(xa_file, here("analysis", "data", "raw_data", "xavier_daily_0.25x0.25.csv"), format = "file"),
   xa_raw = read_csv(xa_file),
   xa_spei = calc_spei_xa(xa_raw),
   xa_lag = lag_spei(xa_spei, maxlag),
   
+  # Prep demographic data
   tar_target(demog_file, here("analysis", "data", "raw_data", "Ha_survey_with_Zombies.csv"), format = "file"),
   demog_raw = read_fix_demog(demog_file),
   demog_surv = add_surv(demog_raw),
@@ -25,12 +27,31 @@ tar_plan(
   model_data = filter_data(demog_spei),
   model_data_cf = filter(model_data, habitat == "CF"),
   model_data_1ha = filter(model_data, habitat == "1-ha"),
+  
+  # Fit demographic models
   s_cf = fit_surv(model_data_cf),
   s_1ha = fit_surv(model_data_1ha),
   g_cf = fit_growth(model_data_cf),
   g_1ha = fit_growth(model_data_1ha),
   f_cf = fit_flwr(model_data_cf),
   f_1ha = fit_flwr(model_data_1ha),
+  
+  # Validate and summarize results
   tar_render(validate_models, "doc/validate_models.Rmd"),
-  tar_render(model_summary, "doc/model_summary.Rmd")
+  tar_render(model_summary, "doc/model_summary.Rmd"),
+  
+  # Descriptive / Exploratory Data Analysis Figures
+  normals = normals_data(),
+  normals_plot = plot_normals(normals),
+  plot_dates = get_plot_daterange(model_data, maxlag),
+  
+  eda_spei = plot_eda_spei(model_data, xa_lag, plot_dates),
+  eda_surv = plot_eda_surv_cohort(demog_done, plot_dates),
+  eda_size = plot_eda_size(model_data, plot_dates),
+  eda_flwr = plot_eda_flwr(model_data, plot_dates),
+  eda_plot = plot_eda_combine(eda_size, eda_surv, eda_flwr, eda_spei)
+  
+  # Model output figures
+  
+  
 )
