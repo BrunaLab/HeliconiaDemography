3dab27236ab2de4b2ad54e5a92a6e2a38baefc66
Aariq <scottericr@gmail.com>
2021-03-15 17:35:26 -0400

    remove tests for removed functions

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,24 +1,6 @@
-# library(here)
-# source(here("R", "utils.R"))
-library(dplyr)
-test_that("matrix iteration works", {
-  A <- matrix(1:9, nrow = 3)
-  expect_equal(A %*% A, A %^% 2)
-  expect_equal(A %*% A %*% A, A %^% 3)
-})
-
-test_that("eu_dist() works", {
-  x = c(0,0)
-  y = c(3,3)
-  expect_equal(eu_dist(x, y), sqrt(3^2 + 3^2))
-})
-
-test_that("nearest() helper works", {
-  x <- seq(1, 10, 0.3)
-  expect_equal(which(nearest(x, 5)), 14)
-  expect_equal(x[nearest(x, 5)], 4.9)
-  expect_s3_class(tibble(x) %>% filter(nearest(x, 5)), "tbl_df")
-})
+library(here)
+source(here("packages.R"))
+source(here("R", "utils.R"))
 
 test_that("as_living() util works", {
   alive <- c(1,2,NA,3,5,NA)
