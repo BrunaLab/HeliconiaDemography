297b87b87cace3a1fbc0ff622f26db01f1fd0025
Aariq <scottericr@gmail.com>
2021-05-11 11:50:30 -0400

    change panel b to time series of proprtion survived

--- _targets.R
+++ _targets.R
@@ -86,7 +86,7 @@ tar_plan(
   eda_surv = plot_eda_surv_cohort(demog_done, plot_dates),
   eda_size = plot_eda_size(model_data, plot_dates),
   eda_flwr = plot_eda_flwr(model_data, plot_dates),
-  eda_plot = plot_eda_combine(eda_size, eda_surv, eda_flwr, eda_spei),
+  eda_plot = plot_eda_combine(eda_size, eda_surv_ts, eda_flwr, eda_spei),
   
   # Model output figures
 
