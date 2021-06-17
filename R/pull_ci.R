726843389ec317cc5dbf9a428d515d4a3f07e227
Aariq <scottericr@gmail.com>
2021-06-17 16:28:57 -0400

    refactored pull_ci()

--- R/pull_ci.R
+++ R/pull_ci.R
@@ -5,11 +5,17 @@
 #'
 #' @return formatted estimate [lower, upper]
 #'
-pull_ci <- function(model, conf.level = 0.84) {
+pull_ci <- function(model, conf.level = 0.84, digits = 3, type = c("response", "link")) {
   linkinv <- model$family$linkinv
-  ci <-
-    broom::tidy(model, conf.int = TRUE, parametric = TRUE, conf.level = conf.level) %>%
-    mutate(across(c(estimate, conf.low, conf.high), ~linkinv(.x) %>% round(2))) %>%
-    mutate(ci = glue::glue("{estimate} [{conf.low}, {conf.high}]"))
-  return(ci$ci[1])
+  type <- match.arg(type)
+  intercept <- coef(model)[1]
+  ci <- confint.default(model, parm = "(Intercept)", level = conf.level)
+  if (type == "response") {
+    intercept <- linkinv(intercept)
+    ci <- linkinv(ci)
+  }
+  intercept <- round(intercept, digits)
+  ci <- round(ci, digits)
+  ci_string <- glue::glue("{intercept} [{ci[1]}, {ci[2]}]")
+  return(ci_string)
 }
